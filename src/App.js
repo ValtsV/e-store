d9a164c2333b2d7a48fd86317476a416d7bcd235
Valts Vilcans <valts03@inbox.lv>
2020-08-20 18:27:51 +0200

    header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage.comp";
 import ShopPage from "./pages/shop.component";
+import Header from "./components/header/header.comp";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
---
+++ src/components/header/header.comp.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./header.styles.scss";
+
+const Header = () => (
+  <div className="bg-pink padding-sides">
+    <div className="header bg-pink">
+      <Link to="/">logo</Link>
+      <ul>
+        <li>
+          <Link to="/shop">shop</Link>
+        </li>
+        <li>
+          <Link to="signin">sign in | register</Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+);
+
+export default Header;
