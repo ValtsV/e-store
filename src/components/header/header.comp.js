4246acd8bd86bc8caa34b7ce0762744d1fe836a9
Valts Vilcans <valts03@inbox.lv>
2020-08-21 20:34:38 +0200

    sign out with google

--- src/components/header/header.comp.js
+++ src/components/header/header.comp.js
@@ -2,8 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "./header.styles.scss";
+import { auth } from "../../firebase/firebase.utils";
+import "../../scss/_config.styles.scss";
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="bg-pink padding-sides">
     <div className="header bg-pink">
       <Link to="/">logo</Link>
@@ -11,9 +13,17 @@ const Header = () => (
         <li>
           <Link to="/shop">shop</Link>
         </li>
-        <li>
-          <Link to="/signin">sign in | register</Link>
-        </li>
+        {currentUser ? (
+          <li>
+            <div className="pointer" onClick={() => auth.signOut()}>
+              sign out
+            </div>
+          </li>
+        ) : (
+          <li>
+            <Link to="/signin">sign in | register</Link>
+          </li>
+        )}
       </ul>
     </div>
   </div>
